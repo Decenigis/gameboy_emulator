9f2885fcb293235c36e9be05778821a6b3061a7f
Decenigis <decenigis@gmail.com>
2025-01-25 23:16:49 +0000

    Background renderer

--- assets/graphics/shaders/ui.fsh
+++ assets/graphics/shaders/ui.fsh
@@ -5,18 +5,33 @@ layout (location = 0) out vec4 frag_colour;
 in vec2 texCoords;
 in vec2 actualCoords;
 
-uniform sampler2D texture0;
+uniform usampler2D bgMap;
+uniform usampler3D tileMapBank0;
+uniform usampler3D tileMapBank1;
+
 uniform ivec2 screenPos;
+uniform bool tileBank;
+
+
+bool useBank1(uint tileId) {
+	return tileId >= 128u;
+}
 
 void main()
 {
 	ivec2 actualCoordsInt = ivec2(actualCoords + screenPos) % 256;
-	ivec2 tile = ivec2(actualCoordsInt / 8);
+	ivec2 tile_position = ivec2(actualCoordsInt >> 3);
+	ivec2 tile_coords = actualCoordsInt % 8;
 
-	vec4 sampledColor = texture(texture0, texCoords);
+	uint tileId = texelFetch(bgMap, tile_position, 0).r ;
 
-	if (sampledColor.a < 0.005)
-		discard;
+	uint sampledLowByte = texelFetch(useBank1(tileId) ? tileMapBank1 : tileMapBank0, ivec3(0, tile_coords.y, tileId % 128u), 0).r % 256u;
+	uint sampledHighByte = texelFetch(useBank1(tileId) ? tileMapBank1 : tileMapBank0, ivec3(1, tile_coords.y, tileId % 128u), 0).r % 256u;
 
-	frag_colour = vec4(float(tile.x) / 32.0, float(tile.y) / 32.0, 0.0, 1.0 );
+	bool isLightPixel = ((int(sampledLowByte) << tile_coords.x) & 128) != 0;
+	bool isDarkPixel = ((int(sampledHighByte) << tile_coords.x) & 128) != 0;
+
+	int colour = (isLightPixel ? 0 : 1) + (isDarkPixel ? 0 : 2);
+
+	frag_colour = vec4(vec3(colour / 3.0), 1.0);
 }
--- src/app/app.rs
+++ src/app/app.rs
@@ -2,8 +2,9 @@ use std::cell::RefCell;
 use std::rc::Rc;
 use dec_gl::{FrameBuffer, GLHandler, Renderable, UICamera, Vertex2d};
 use dec_gl::shader::{ShaderManager, ShaderProgram};
-use dec_gl::texture::{Texture2D, TextureManager};
-use glm::ivec2;
+use dec_gl::texture::{Texture2D, Texture2Du8, Texture3Du8, TextureManager};
+use glm::{ivec2, ivec3};
+
 
 pub struct App {
     pub _args: Vec<String>,
@@ -12,7 +13,6 @@ pub struct App {
 
     gl_handler: Rc<RefCell<GLHandler>>,
     framebuffer: FrameBuffer,
-    texture_manager: TextureManager,
     shader_manager: ShaderManager
 }
 
@@ -40,20 +40,64 @@ impl App {
 
             gl_handler,
             framebuffer,
-            texture_manager,
             shader_manager
         }
      }
+
+
 
     pub fn run (&mut self) {
         match self.shader_manager.bind("UI".to_string()) {
             Ok(shader) => {
                 shader.set_uniform("pv".to_string(), self.camera.get_matrix());
+
+                shader.set_uniform("bgMap".to_string(), 0);
+                shader.set_uniform("tileMapBank0".to_string(), 1);
+                shader.set_uniform("tileMapBank1".to_string(), 2);
+
                 shader
             }
             Err(_) => return
         };
-        self.texture_manager.bind("test".to_string());
+
+        let mut bg_map = Texture2Du8::new();
+        let mut tilemap_bank_0 = Texture3Du8::new();
+        let mut tilemap_bank_1 = Texture3Du8::new();
+        //let mut tilemap_bank_2 = Texture3Du8::new();
+
+        let mut bg_map_data = vec![0; 1024];
+        let mut tilemap_bank_0_data = vec![0; 2048];
+        let tilemap_bank_1_data = vec![0; 2048];
+        // let mut tilemap_bank_2 = vec![0; 32 * 32];
+
+        for i in 0..16 {
+            tilemap_bank_0_data[16 + i] = 85 << (i % 2) ;
+        }
+
+        for x in 0..=10 {
+            for y in 0..=10 {
+                bg_map_data[4 + x + (3 + y) * 32] = 1;
+            }
+        }
+
+        bg_map.set_data(
+            &bg_map_data,
+            ivec2(32, 32)).unwrap();
+
+        bg_map.bind_to_unit(0);
+
+        tilemap_bank_0.set_data(
+            &tilemap_bank_0_data,
+            ivec3(2, 8, 128)).unwrap();
+
+        tilemap_bank_0.bind_to_unit(1);
+
+        tilemap_bank_1.set_data(
+            &tilemap_bank_1_data,
+            ivec3(2, 8, 128)).unwrap();
+
+        tilemap_bank_1.bind_to_unit(2);
+
 
         let renderable = Renderable::new_initialised(&vec![
             Vertex2d { x: 0.0, y: 0.0, u: 0.0, v: 0.0},
@@ -63,10 +107,9 @@ impl App {
             Vertex2d { x: 0.0, y: 144.0, u: 0.0, v: 1.0},
             Vertex2d { x: 160.0, y: 0.0, u: 1.0, v: 0.0},
             Vertex2d { x: 160.0, y: 144.0, u: 1.0, v: 1.0},
-        ],
-                                                     None).unwrap();
+        ],None).unwrap();
 
-        let mut pos = ivec2(0, 0);
+        let pos = ivec2(0, 0);
 
         while !self.gl_handler.borrow().wind_should_close() {
             for _event in self.gl_handler.borrow_mut().handle_events() {
@@ -79,15 +122,10 @@ impl App {
 
             match self.shader_manager.bind("UI".to_string()) {
                 Ok(shader) => {
-                    shader.set_uniform("screenPos".to_string(),pos);
+                    shader.set_uniform("screenPos".to_string(), pos);
                 }
                 Err(_) => return
             };
-            pos.x += 1;
-            if pos.x == 256 {
-                pos.y += 1;
-                pos.x = 0;
-            }
 
             renderable.draw();
 
