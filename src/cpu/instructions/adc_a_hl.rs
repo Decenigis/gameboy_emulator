5ab8bdace1c1524a8118a4b5ffb013fd50a460a0
Decenigis <decenigis@gmail.com>
2025-08-01 11:10:46 +0100

    adc_a_hl

---
+++ src/cpu/instructions/adc_a_hl.rs
@@ -0,0 +1,84 @@
+use crate::cpu::alu::ALU;
+use crate::cpu::instructions::Instruction;
+use crate::cpu::register::Register;
+use crate::cpu::register8::Register8;
+use crate::cpu::registers::Registers;
+use crate::memory::{MemoryController, MemoryTrait};
+use parking_lot::Mutex;
+use std::sync::Arc;
+
+pub struct AdcAHl {
+    counter: u8,
+    value: u8
+}
+
+
+impl Instruction for AdcAHl {
+
+    #[inline]
+    fn from_opcode(opcode: &u8) -> Option<Box<dyn Instruction>> {
+        if *opcode == 0x8E {
+            return Some(Box::new(AdcAHl {
+                counter: 1,
+                value: 1
+            }))
+        }
+        None
+    }
+
+
+    fn get_opcode(&self) -> u8 {
+        0x8E
+    }
+
+    fn act(&mut self, registers: &mut Registers, alu: &mut ALU, memory_controller: Arc<Mutex<MemoryController>>, _enable_interrupts: &mut bool, _is_halted: &mut bool) -> bool {
+        if self.counter == 1 {
+            self.value = memory_controller.lock().get(registers.hl.get_value());
+        }
+        else if self.counter == 0 {
+            alu.adc(&mut *registers.a.clone().borrow_mut(), &Register8::new(self.value));
+            return true;
+        }
+
+        self.counter -= 1;
+        false
+    }
+}
+
+
+#[cfg(test)]
+mod adc_a_a {
+    use super::*;
+
+    reusable_testing_macro!(0x8E, AdcAHl);
+
+    #[test]
+    fn sub_a_from_n_on_tick_1() {
+        let mut registers = Registers::new(0, 0, 0, 0xD000, 0xC000, 0);
+        let memory = Arc::new(Mutex::new(MemoryController::new()));
+        let mut alu = ALU::new(registers.f.clone());
+
+        memory.lock().set(0xD000, 0x12);
+
+        let mut instruction = AdcAHl { counter: 1, value:0 };
+
+        let result = instruction.act(&mut registers, &mut alu, memory.clone(), &mut false, &mut false);
+
+        assert_eq!(false, result);
+        assert_eq!(0x12, instruction.value);
+    }
+
+    #[test]
+    fn sub_a_from_n_on_tick_0() {
+        let mut registers = Registers::new(0x2400, 0, 0, 0, 0xC001, 0);
+        let memory = Arc::new(Mutex::new(MemoryController::new()));
+        let mut alu = ALU::new(registers.f.clone());
+
+        let mut instruction = AdcAHl { counter: 0, value: 0x12 };
+
+        let result = instruction.act(&mut registers, &mut alu, memory.clone(), &mut false, &mut false);
+
+        assert_eq!(true, result);
+        assert_eq!(0x36, registers.a.borrow().get_value());
+    }
+}
